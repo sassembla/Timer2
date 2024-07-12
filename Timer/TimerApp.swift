c701e055e3ab5f36e32474f6a5dd456a7e8b93b5
Toru <toru.inoue@kissaki.tv>
2024-07-13 02:02:28 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by aimer on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
