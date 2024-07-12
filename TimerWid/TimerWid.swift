09b932831502fa5db2eec497812294ce7743b7dc
Toru <toru.inoue@kissaki.tv>
2024-07-13 03:13:21 +0900

    途中

--- TimerWid/TimerWid.swift
+++ TimerWid/TimerWid.swift
@@ -5,8 +5,8 @@
 //  Created by aimer on 2024/07/13.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
@@ -16,17 +16,12 @@ struct Provider: AppIntentTimelineProvider {
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: configuration)
     }
-    
+
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
+        let entry = SimpleEntry(date: .now, configuration: configuration)
+        entries.append(entry)
 
         return Timeline(entries: entries, policy: .atEnd)
     }
@@ -37,48 +32,82 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationAppIntent
 }
 
-struct TimerWidEntryView : View {
+struct TimerWidEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
         VStack {
-            Text("Time:")
+            Text("Timbbbaaaa:")
             Text(entry.date, style: .time)
 
             Text("Favorite Emoji:")
             Text(entry.configuration.favoriteEmoji)
         }
+        .containerBackground(.fill.tertiary, for: .widget)
+    }
+}
+
+struct Calculator_WidgetEntryView2: View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Timbbbaaaa:")
+            Text(entry.date, style: .time)
+
+            Text("Favorite Emoji:")
+            Text(entry.configuration.favoriteEmoji)
+        }
+        .containerBackground(.fill.tertiary, for: .widget)
     }
 }
 
 struct TimerWid: Widget {
-    let kind: String = "TimerWid"
+    let kind: String = "Calculator_Widget"
 
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             TimerWidEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
         }
     }
 }
 
-extension ConfigurationAppIntent {
-    fileprivate static var smiley: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "😀"
-        return intent
-    }
-    
-    fileprivate static var starEyes: ConfigurationAppIntent {
-        let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "🤩"
-        return intent
-    }
-}
+// private extension ConfigurationAppIntent {
+//    static var smiley: ConfigurationAppIntent {
+//        let intent = ConfigurationAppIntent()
+//        intent.favoriteEmoji = "😀"
+//        return intent
+//    }
+//
+//    static var starEyes: ConfigurationAppIntent {
+//        let intent = ConfigurationAppIntent()
+//        intent.favoriteEmoji = "🤩"
+//        return intent
+//    }
+// }
 
-#Preview(as: .systemSmall) {
-    TimerWid()
-} timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
-    SimpleEntry(date: .now, configuration: .starEyes)
-}
+// #Preview(as: .systemSmall) {
+//    TimerWid()
+// } timeline: {
+//    SimpleEntry(date: .now, configuration: .smiley)
+//    SimpleEntry(date: .now, configuration: .starEyes)
+// }
+
+// extension View {
+//    func widgetBackground(_ backgroundView: some View) -> some View {
+////        if #available(iOSApplicationExtension 17.0, *) {
+//        return containerBackground(for: .widget) {
+//            backgroundView
+//        }
+////        } else {
+////            return background(backgroundView)
+////        }
+//    }
+// }
+
+// struct MyWidget_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MyWidgetEntryView(entry: SimpleEntry(date: Date()))
+//            .previewContext(WidgetPreviewContext(family: .systemSmall))
+//    }
+// }
--- TimerWid/TimerWidBundle.swift
+++ TimerWid/TimerWidBundle.swift
@@ -5,13 +5,13 @@
 //  Created by aimer on 2024/07/13.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 @main
 struct TimerWidBundle: WidgetBundle {
     var body: some Widget {
         TimerWid()
-        TimerWidLiveActivity()
+//        TimerWidLiveActivity()
     }
 }
