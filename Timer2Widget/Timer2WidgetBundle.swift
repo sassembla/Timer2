05221a62b3131f683571bec7a7527e4d821cc793
Toru <toru.inoue@kissaki.tv>
2024-07-13 11:09:09 +0900

    動いたーーーうーん、初期設定を覆せないケースがあるっぽい

---
+++ Timer2Widget/Timer2WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Timer2WidgetBundle.swift
+//  Timer2Widget
+//
+//  Created by aimer on 2024/07/13.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Timer2WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Timer2Widget()
+    }
+}
